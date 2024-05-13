9475c99756d030fa2546e1548716facdbe5ece1e
syifaanuraini <syifanuraini4142@gmail.com>
2024-05-13 07:16:39 +0700

    Add files via upload

---
+++ coba daspro 1.pas
@@ -0,0 +1,11 @@
+PROGRAM Jarak;
+Var
+  v,t : integer
+  s : integer
+
+Begin
+  writeln('Masukkan nilai v : '); readln(v);
+  writeln('Masukkan nilai t : '); readln(t);
+  s:=v*t;
+  writeln('Maka jarak yang ditempuh mobil adalah sejauh ',s);
+End.
